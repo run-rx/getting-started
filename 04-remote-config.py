3444f44ed15aef26cee8246ab2d183915733b1f2
Kris Chodorow <k.chodorow@gmail.com>
2023-05-09 11:11:57 -0400

    Change remote config example to Python 3.7

--- 04-remote-config.py
+++ 04-remote-config.py
@@ -10,37 +10,44 @@ instance.
 However, the files in .rx/remotes are simply starter templates and you can
 create any configuration you want.
 
-For example, try running this file (04-remote-config.py) with the remote
-instance you've been using so far. This file is valid Python 2 code, but not
-Python 3, so You should see something like:
-
-  $ rx python 04-remote-config.py
-    File "/Users/k/gitroot/rx/samples/04-remote-config.py", line 14
-      print "Hello from Python 2."
-      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-  SyntaxError: Missing parentheses in call to 'print'. Did you mean print(...)?
-
-rx can pull any public docker image, so we'll create a remote machine with
-Python 2. Create `.rx/remotes/python2-config` with the following contents:
+For example, this file uses removeprefix, which is a method added in Python
+3.9. Try running this with Python 3.7 by creating `.rx/remotes/py37` with
+the following content:
 
   {
     "image": {
-      "docker": "python:2.7.7"
+      "docker": "python:3.7-slim"
     }
   }
 
-Now tell rx that you want to use a different remote machine by running `rx init`
-again:
+Now tell rx that you want to use a different remote machine configuration by
+running `rx init` again, this time with the --remote flag:
 
-  $ rx init --remote=python2-config
+  $ rx init --remote=py37
 
 Now if you run this script, you should see:
 
   $ rx python 04-remote-config.py
-  Hello from Python 2.
+  Some Python projects:
+  Traceback (most recent call last):
+    File "04-remote-config.py", line 44, in <module>
+      print(proj.removesuffix('Py'))
+  AttributeError: 'str' object has no attribute 'removesuffix'
 
-To switch back to Python 3, run `rx init` again without any flags and it'll go
-back to using the config `default` is symlinked to.
+To upgrade back to Python 3.11, run `rx init` again without any flags and it'll
+go back to using the config `default` is symlinked to. Now it prints:
+
+  $ rx python 04-remote-config.py
+  Some Python projects:
+  Torch
+  Pi
+  Mongo
+
+In the future these remote config files will be used to specify hardware
+requirements, too.
 """
 
-print "Hello from Python 2."
+PY_PROJECTS = ['PyTorch', 'PyPi', 'PyMongo']
+print('Some Python projects:')
+for proj in PY_PROJECTS:
+  print(proj.removeprefix('Py'))
